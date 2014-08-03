cce8adc0dce17eac8459114c380260227de49e7c
Alfredo Canziani <alfredo.canziani@gmail.com>
2014-08-03 17:33:37 -0400

    Added ZCA to code

--- PCA/src/PCA.lua
+++ PCA/src/PCA.lua
@@ -2,13 +2,24 @@
 -- PCA with Torch7
 --------------------------------------------------------------------------------
 -- Koray Kavukcuoglu (https://github.com/koraykv/unsup)
--- Alfredo Canziani, Jul 14
+-- Alfredo Canziani, Jul, Aug 14
 --------------------------------------------------------------------------------
 
 -- Instruction -----------------------------------------------------------------
 -- This scripts aims to provide an understanding about how to play with PCA in
 -- order to maniplate data dimensionality (for algorithmic speed or visualisa-
 -- tion). Furthermore, it shows how to perform ZCA and PCA whitening.
+
+-- Choose a spherification radius (in normal application is set to 1, but bigger
+-- values will look better in the chart and enable ZCA display
+--<<<
+radius = 3 -- 1, 3 for visualisation sake
+showZCA = false -- true/false
+-->>>
+
+-- You want, perhaps, also to try to enable and disable the visualisation of the
+-- rotated data and PCA whitening in the ZCA visualisation section below (line
+-- 84 and below)
 
 -- Requires --------------------------------------------------------------------
 require 'gnuplot'
@@ -51,7 +62,7 @@ print(b .. 'sqrt of the above (energy/std):' .. n); print(torch.sqrt(s))
 -- Projection ------------------------------------------------------------------
 X_hat = (X - torch.ones(m,1) * mean) * v[{ {},{1} }] -- m x 1
 
--- Visualising -----------------------------------------------------------------
+-- Visualising PCA -------------------------------------------------------------
 vv = v * torch.diag(torch.sqrt(s))
 vv = torch.cat(torch.ones(2,1) * mean, vv:t())
 
@@ -64,3 +75,22 @@ gnuplot.plot{
 gnuplot.axis('equal')
 gnuplot.axis{-20,50,-10,30}
 gnuplot.grid(true)
+
+-- ZCA / spherification / whitening --------------------------------------------
+X_rot = (X - torch.ones(m,1) * mean) * v
+X_PCA_white = X_rot * torch.sqrt(s):pow(-1):mul(radius):diag()
+X_ZCA_white = X_PCA_white * v:t()
+
+-- Visualising ZCA -------------------------------------------------------------
+if showZCA then
+   gnuplot.figure(2)
+   gnuplot.plot{
+      {'dataset',X,'+'},
+--    {'rortated',X_rot,'+'},
+--    {'PCA white',X_PCA_white,'+'},
+      {'ZCA white',X_ZCA_white,'+'}
+   }
+   gnuplot.axis('equal')
+   gnuplot.axis{-20,50,-10,30}
+   gnuplot.grid(true)
+end
